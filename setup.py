2121c8bed67f6318668e6a2ecfc5091d5709e010
Kenil-kavar <kenilkavar@gmail.com>
2023-11-17 16:59:57 +0530

    Still need modification

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Kenil Kavar",
+    author_email="kenilkavar@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ src/constant/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "pwskills"
+MONGO_COLLECTION_NAME = "waferfault"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://kenilkavar:Kenilkavar@cluster0.azdvmbj.mongodb.net/?retryWrites=true&w=majority"
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
